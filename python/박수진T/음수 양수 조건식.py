60aface8dbc42f999c216f2448605517598252e7
Dongeon Yoon <0125ehddjs@gmail.com>
2025-04-17 16:17:40 +0900

    조건식 추가

---
+++ python/박수진T/음수 양수 조건식.py
@@ -0,0 +1,7 @@
+num=int(input())
+if num>0:
+    print('양수')
+elif num<0:
+    print('음수')
+else:
+    print('zero')
---
+++ python/박수진T/짝수 홀수 조건식.py
@@ -0,0 +1,5 @@
+a=int(input('숫자를 입력하세요'))
+if a%2==0:
+    print('짝수')
+else:
+    print('홀수')
